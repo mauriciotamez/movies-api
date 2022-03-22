0fe6366f5cc917cbf0a3dd25617dacf7fc350a10
Mauricio Tamez <vitxlssreturn@gmail.com>
2022-03-22 00:06:49 -0600

    Copied movies and actors functions from main to current branch

--- controllers/actors.controller.js
+++ controllers/actors.controller.js
@@ -3,22 +3,125 @@ const { filterObj } = require('../util/filterObj');
 const { handleError } = require('../util/handleError');
 const { AppError } = require('../util/appError');
 
+// Models
+const { Actor } = require('../models/actor.model');
+
 exports.getAllActors = handleError(
-  async (req, res, next) => {}
+  async (req, res, next) => {
+    const actors = await Actor.findAll({
+      where: { status: 'active' }
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: { actors }
+    });
+  }
 );
 
 exports.getActorByID = handleError(
-  async (req, res, next) => {}
+  async (req, res, next) => {
+    const { id } = req.params;
+
+    const actor = await Actor.findOne({
+      where: { status: 'active', id }
+    });
+
+    if (!actor) {
+      return next(
+        new AppError(
+          400,
+          'Cannot find an actor, invalid ID'
+        )
+      );
+    }
+    res.status(200).json({
+      status: 'success',
+      data: { actor }
+    });
+  }
 );
 
 exports.createActor = handleError(
-  async (req, res, next) => {}
+  async (req, res, next) => {
+    const { name, country, age, profilePic } = req.body;
+
+    if (!name || !country || !age) {
+      return next(
+        new AppError(
+          400,
+          'Must provide a name, country age and profile pic.'
+        )
+      );
+    }
+
+    const actor = await Actor.create({
+      name,
+      country,
+      age,
+      profilePic
+    });
+
+    res.status(201).json({
+      status: 'success',
+      data: { actor }
+    });
+  }
 );
 
 exports.updateActor = handleError(
-  async (req, res, next) => {}
+  async (req, res, next) => {
+    const { id } = req.params;
+
+    const data = filterObj(
+      req.body,
+      'name',
+      'country',
+      'age'
+    );
+
+    const actor = await Actor.findOne({
+      where: { id, status: 'active' }
+    });
+
+    if (!actor) {
+      return next(
+        new AppError(
+          400,
+          'Cannot update actor, invalid ID.'
+        )
+      );
+    }
+
+    await actor.update({ ...data });
+
+    res
+      .status(204)
+      .json({ status: 'success', data: { actor } });
+  }
 );
 
 exports.deleteActor = handleError(
-  async (req, res, next) => {}
+  async (req, res, next) => {
+    const { id } = req.params;
+
+    const actor = await Actor.findOne({
+      where: { id, status: 'active' }
+    });
+
+    if (!actor) {
+      return next(
+        new AppError(
+          400,
+          'Cannot delete actor, invalid ID.'
+        )
+      );
+    }
+
+    await actor.update({ status: 'deleted' });
+
+    res.status(204).json({
+      status: 'success'
+    });
+  }
 );
--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -3,22 +3,119 @@ const { filterObj } = require('../util/filterObj');
 const { handleError } = require('../util/handleError');
 const { AppError } = require('../util/appError');
 
+// Models
+const { Movie } = require('../models/movie.model');
+
 exports.getAllMovies = handleError(
-  async (req, res, next) => {}
+  async (req, res, next) => {
+    const movies = await Movie.findAll({
+      where: { status: 'active' }
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: { movies }
+    });
+  }
 );
 
 exports.getMovieByID = handleError(
-  async (req, res, next) => {}
+  async (req, res, next) => {
+    const { id } = req.params;
+
+    const movie = await Movie.findOne({
+      where: { status: 'active', id }
+    });
+
+    if (!movie) {
+      return next(
+        new AppError(404, 'Cannot find movie, invalid ID')
+      );
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: { movie }
+    });
+  }
 );
 
 exports.createMovie = handleError(
-  async (req, res, next) => {}
+  async (req, res, next) => {
+    const { title, description, duration, genre } =
+      req.body;
+
+    if (!title || !description || !duration || !genre) {
+      return next(
+        new AppError(
+          404,
+          'Need to provide a title, description, duration and genre'
+        )
+      );
+    }
+
+    const movie = await Movie.create({
+      title,
+      description,
+      duration,
+      genre
+    });
+
+    res.status(201).json({
+      status: 'success',
+      data: { movie }
+    });
+  }
 );
 
 exports.updateMovie = handleError(
-  async (req, res, next) => {}
+  async (req, res, next) => {
+    const { id } = req.params;
+
+    const data = filterObj(
+      req.body,
+      'title',
+      'description',
+      'duration',
+      'genre'
+    );
+
+    const movie = await Movie.findOne({
+      where: { id, status: 'active' }
+    });
+
+    if (!movie) {
+      return next(400, 'Cannot update movie, invalid ID');
+    }
+
+    await movie.update({ ...data });
+
+    res
+      .status(204)
+      .json({ status: 'success', data: { movie } });
+  }
 );
 
 exports.deleteMovie = handleError(
-  async (req, res, next) => {}
+  async (req, res, next) => {
+    const { id } = req.params;
+
+    if (!id) {
+      res.status(404).json({
+        status: 'error',
+        message: 'Cannot delete movie, invalid ID'
+      });
+      return;
+    }
+
+    const movie = await Movie.findOne({
+      where: { status: 'active', id }
+    });
+
+    await movie.update({ status: 'deleted' });
+
+    res.status(200).json({
+      status: 'success'
+    });
+  }
 );
