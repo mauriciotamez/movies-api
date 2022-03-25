6cac92b28540bd62e04b2427ffe9bd1c3192ec58
Mauricio Tamez <vitxlssreturn@gmail.com>
2022-03-24 18:12:18 -0600

    Our global error handler is now working.

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const express = require('express');
+const express = require('express')
 
+// Utils
+const {
+	globalErrorHandler
+} = require('./controllers/error.controller')
 // Routes
-const { moviesRouter } = require('./routes/movies.router');
-const { usersRouter } = require('./routes/users.route');
-const { actorsRouter } = require('./routes/actors.route');
+const { moviesRouter } = require('./routes/movies.router')
+const { usersRouter } = require('./routes/users.route')
+const { actorsRouter } = require('./routes/actors.route')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
 // Endpoints
-app.use('/api/v1/movies', moviesRouter);
-app.use('/api/v1/users', usersRouter);
-app.use('/api/v1/actors', actorsRouter);
+app.use('/api/v1/movies', moviesRouter)
+app.use('/api/v1/users', usersRouter)
+app.use('/api/v1/actors', actorsRouter)
 
-module.exports = { app };
+app.use(globalErrorHandler)
+
+module.exports = { app }
