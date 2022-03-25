5548b06edefc61a2cab5ba5d7e6170a6c246787b
Mauricio Tamez <vitxlssreturn@gmail.com>
2022-03-24 19:17:25 -0600

    We are no longer using our initModels() fn, since we are doing relations manually with the migrations.

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
 const { app } = require('./app');
 
 // Models
-const { Actor } = require('./models/actor.model');
-const {
-  ActorsInMovies
-} = require('./models/actorsInMovies');
-const { Movie } = require('./models/movie.model');
-const { Review } = require('./models/review.model');
-const { User } = require('./models/user.model');
 
 // Utils
 const { sequelize } = require('./util/database');
-const { initModels } = require('./util/initModels');
 
 sequelize
   .authenticate()
   .then(() => console.log('Database authenticated'))
   .catch((err) => console.log(err));
-
-// Models relations
-initModels();
 
 // Actor.sync()
 //   .then(() => console.log('database synced'))
